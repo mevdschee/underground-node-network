a017e99c20a6fb535fbd261f824c6d061d8b4b75
Maurits van der Schee <maurits@vdschee.nl>
2026-01-29 01:47:29 +0100

    update

--- internal/sshserver/server.go
+++ internal/sshserver/server.go
@@ -742,13 +742,13 @@ func (s *Server) handleInternalCommand(p *Person, cmd string) bool {
 
 			if s.isOperator(p.PubKey) {
 				addMessage("--- Operator Commands ---", ui.MsgServer)
-				addMessage("/kick <user/hash> [reason] - Kick a person", ui.MsgServer)
-				addMessage("/kickban <user/hash> [reason] - Ban a person", ui.MsgServer)
-				addMessage("/unban <hash>              - Unban a person", ui.MsgServer)
-				addMessage("/banlist                   - List banned people", ui.MsgServer)
-				addMessage("/lock <key>                - Lock the room", ui.MsgServer)
-				addMessage("/unlock                    - Unlock the room", ui.MsgServer)
-				addMessage("/kickall [reason]          - Kick everyone", ui.MsgServer)
+				addMessage("/kick <user> [reason]    - Kick a person", ui.MsgServer)
+				addMessage("/kickban <user> [reason] - Ban a person", ui.MsgServer)
+				addMessage("/unban <user>            - Unban a person", ui.MsgServer)
+				addMessage("/banlist                 - List banned people", ui.MsgServer)
+				addMessage("/lock <key>              - Lock the room", ui.MsgServer)
+				addMessage("/unlock                  - Unlock the room", ui.MsgServer)
+				addMessage("/kickall [reason]        - Kick everyone", ui.MsgServer)
 			}
 			return true
 		case "who":
